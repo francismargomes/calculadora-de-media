39bbbf24ca458c18549ac4eb292e15114e4017d3
Francismar Gomes <77031054+francismargomes@users.noreply.github.com>
2022-03-19 17:32:02 -0400

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,15 @@
+var nome = "Francismar"
+var notaPrimeiroBimestre = 9.548
+var notaSegundoBimestre = 7.543
+var notaTerceiroBimestre = 4.756
+var notaQuartoBimestre = 2.234
+  
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem-vindo " + nome)
+console.log(notaFixada)
+
+// Revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
